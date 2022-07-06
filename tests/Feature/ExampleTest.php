dd464c00919c6e5951fc3b8039d71ea38e698402
hiroshi.kitahara <hiroshi.kitahara@hiroshikitaharanoMacBook-Pro.local>
2022-07-06 12:14:32 +0900

    fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,8 @@
 namespace Tests\Feature;
 
 use App\Admin;
-use App\User;
 use Tests\TestCase;
 use App\Organization;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
