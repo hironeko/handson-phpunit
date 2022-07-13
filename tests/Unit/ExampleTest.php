29bc4b6a33f060283fa04a1e19048cab763d6154
hiroshi.kitahara <hiroshi.kitahara@hiroshikitaharanoMacBook-Pro.local>
2022-07-13 17:27:12 +0900

    fix

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_sample()
     {
         $this->assertTrue(true);
     }
